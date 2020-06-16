31e715bf606ce24ab5510f5d7d718e28009d7ec5
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-06-16 20:36:32 +0200

    crown update nav with hoc withrouter

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'; 
+import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+export const HatsPage = () =>
+  (
+    <div>
+      <h1>HATS PAGES</h1>
+    </div>
+  )
+
 
 function App() {
   return (
-    <div> 
-      <HomePage />
+    <div>
+      <Switch> 
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
