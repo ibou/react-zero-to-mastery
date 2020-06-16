14f40f8ff4c606291662996927552427c69d375f
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-06-17 01:39:21 +0200

    Updates

---
+++ precache-manifest.30cc99b17ed7b785eca77ceee93a5c90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17350e2927f0ad95fed68e7c53dd0d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98eea3b1a3c8558da77b",
+    "url": "/static/css/main.767c377d.chunk.css"
+  },
+  {
+    "revision": "7ab8811cf14123cb5a94",
+    "url": "/static/js/2.a233947a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a233947a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98eea3b1a3c8558da77b",
+    "url": "/static/js/main.7905db47.chunk.js"
+  },
+  {
+    "revision": "405f430893446efaf6a2",
+    "url": "/static/js/runtime-main.9129b20f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
