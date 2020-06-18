c77fc418d324a307e9de4cb1ad26e01e31b515d9
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-06-18 21:39:47 +0200

    Persistore redux

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 
 ReactDOM.render(
@@ -12,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- crwn-clothing/src/redux/store.js
+++ crwn-clothing/src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import { persistStore } from 'redux-persist';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 const middlewares = [logger];
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
-export default store;
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
+
+export const persistor = persistStore(store);
+export default {store, persistor};
