e402c9ee98244acf5a4ca9868007c039704f64b0
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-06-19 03:32:03 +0200

    add collection item, with redux selector

--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './collection-item.styles.scss';
+
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ item, addItem }) => {
+import './collection-item.styles.scss';
 
+const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
+
   return (
     <div className='collection-item'>
       <div
@@ -19,16 +21,22 @@ const CollectionItem = ({ item, addItem }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
+      <CustomButton
+        className='custom-button'
+        onClick={() => addItem(item)}
+        inverted
+      >
+        Add to cart
+      </CustomButton>
     </div>
-  )
+  );
 };
-
-const mapStateToProps = state => ({
-})
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(CollectionItem)
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
