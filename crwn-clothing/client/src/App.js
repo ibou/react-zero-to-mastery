e23fbea3b1e4efaf2d0fd1bc2fb8002825784b0d
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-06-24 01:12:37 +0200

    move appli to client forlder

--- crwn-clothing/client/src/App.js
+++ crwn-clothing/client/src/App.js
@@ -3,26 +3,25 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-
 import Header from './components/header/header.component';
+
+import { GlobalStyle } from './global.styles';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
 const App = ({ checkUserSession, currentUser }) => {
- 
   useEffect(() => {
     checkUserSession();
   }, [checkUserSession]);
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
@@ -32,17 +31,13 @@ const App = ({ checkUserSession, currentUser }) => {
           exact
           path='/signin'
           render={() =>
-             currentUser ? (
-              <Redirect to='/' />
-            ) : (
-                <SignInAndSignUpPage />
-              )
+            currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />
           }
         />
       </Switch>
     </div>
   );
-}
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
