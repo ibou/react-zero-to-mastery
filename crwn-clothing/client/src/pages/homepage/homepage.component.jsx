09fcc915878c319315478f876cad7f4c6e59a6f4
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-06-24 02:00:41 +0200

    add error boundary

--- crwn-clothing/client/src/pages/homepage/homepage.component.jsx
+++ crwn-clothing/client/src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Directory from '../../components/directory/directory.component';
 import { HomePageContainer } from './homepage.styles';
  
-const HomePage = history => {
+const HomePage = history => { 
     return (
         <HomePageContainer>
             <Directory history={history} /> 
